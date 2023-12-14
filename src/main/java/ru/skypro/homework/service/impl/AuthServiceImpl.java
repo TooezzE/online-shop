f2e8525c8b6f0593897f301b70736eca21532806
TooezzE <azazaz.com534@gmail.com>
2023-12-14 13:15:40 +0300

    third week

--- src/main/java/ru/skypro/homework/service/impl/AuthServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/AuthServiceImpl.java
@@ -3,10 +3,10 @@ package ru.skypro.homework.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
 import ru.skypro.homework.dto.Register;
 import ru.skypro.homework.entity.User;
+import ru.skypro.homework.mappers.UserMapper;
 import ru.skypro.homework.repository.UserRepository;
 import ru.skypro.homework.service.AuthService;
 import ru.skypro.homework.service.CustomUserDetailsService;
@@ -16,13 +16,15 @@ public class AuthServiceImpl implements AuthService {
 
     private final CustomUserDetailsService userDetailsService;
     private final PasswordEncoder encoder;
-    @Autowired
+    private final UserMapper mapper;
     private UserRepository repository;
 
     public AuthServiceImpl(CustomUserDetailsService userDetailsService,
-                           PasswordEncoder passwordEncoder) {
+                           PasswordEncoder passwordEncoder, UserMapper mapper, UserRepository repository) {
         this.userDetailsService = userDetailsService;
         this.encoder = passwordEncoder;
+        this.mapper = mapper;
+        this.repository = repository;
     }
 
     @Override
@@ -33,9 +35,7 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public boolean register(Register register) {
-        User user = new User(0, register.getUsername(),
-                encoder.encode(register.getPassword()), register.getFirstName(), register.getLastName(),
-                register.getPhone(), register.getRole(), "hello");
+        User user = mapper.registerToUser(register);
         repository.save(user);
         return true;
     }
