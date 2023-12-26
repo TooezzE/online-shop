308ea10734a85db8e2deecaa5334fc945e3318d1
TooezzE <azazaz.com534@gmail.com>
2023-12-26 21:41:22 +0300

    fourth week

---
+++ src/main/resources/liquibase/scripts/shop.sql
@@ -0,0 +1,39 @@
+-- liquibase formatted sql
+
+-- changeset egor:1
+
+create table comments (
+    id serial primary key,
+    created_at int8,
+    text varchar,
+    ad_id int,
+    author_id int
+);
+
+
+create table users (
+    id serial primary key,
+    email varchar,
+    password varchar,
+    first_name varchar,
+    last_name varchar,
+    phone varchar,
+    role varchar,
+    image_id int
+);
+
+create table ads (
+    id serial primary key,
+    description varchar,
+    price int,
+    title varchar,
+    author_id int,
+    image_id int
+);
+
+create table images (
+    id serial primary key,
+    file_size int8,
+    content_type varchar,
+    "data" bytea NULL
+);
